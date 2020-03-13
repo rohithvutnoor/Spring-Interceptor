f37318cf146a2f6acd2244021e9f35006f1bfc9e
rohithvutnoor <rohithvutnoor@gmail.com>
2020-03-13 20:20:42 +0530

    Removed unnecessary stream objects

--- mock/src/main/java/com/dev/works/configuration/ResponseWrapper.java
+++ mock/src/main/java/com/dev/works/configuration/ResponseWrapper.java
@@ -1,8 +1,6 @@
 package com.dev.works.configuration;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
@@ -11,26 +9,19 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 
 public class ResponseWrapper extends HttpServletResponseWrapper {
 
-	//ByteArrayOutputStream byteArrayOutputStream;
-	//PrintWriter printWriter;
-	
-	//ByteArrayPrinter pw = new ByteArrayPrinter();
 	private String responseBody;
-	
+
 	public ResponseWrapper(HttpServletResponse response) throws IOException {
 		super(response);
 
-		ContentCachingResponseWrapper responseWrapper = (ContentCachingResponseWrapper)response;
-		
-		//byteArrayOutputStream = new ByteArrayOutputStream();
-		
+		ContentCachingResponseWrapper responseWrapper = (ContentCachingResponseWrapper) response;
+
 		String s = new String(responseWrapper.getContentAsByteArray());
 		s = s.replace("{", "{\"name\":\"lenovo\",");
-		//byteArrayOutputStream.write(s.getBytes());
+
 		this.setResponseBody(s);
-		
 	}
-	
+
 	public String getResponseBody() {
 		return responseBody;
 	}
@@ -38,5 +29,5 @@ public class ResponseWrapper extends HttpServletResponseWrapper {
 	public void setResponseBody(String responseBody) {
 		this.responseBody = responseBody;
 	}
-    
+
 }
